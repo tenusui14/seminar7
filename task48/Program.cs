4173cb16bd75de826a87a9d663787a9a82dbf56a
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-30 20:49:02 +0300

    task48

---
+++ task48/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+int Input()
+{
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GetMatrix(int lines, int columns)
+{
+    int[,] matrix = new int[lines, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+
+    }
+
+    return matrix;
+}
+int raws = Input();
+int columns = Input();
+int[,] myMatrix = GetMatrix(raws, columns);
+PrintMatrix(myMatrix);
