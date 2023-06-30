b6f0815e1e894e91f5dc89c75f2855a95b7c3a07
Artem Tsytsylin <workhardgofock@gmail.com>
2023-06-30 21:08:57 +0300

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int Input()
+{
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int[,] GetRandomMatrix(int lines, int columns)
+{
+    int[,] matrix = new int[lines, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10);
+        }
+
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void GetChangedMatrix(int[,]matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 != 0 && j % 2 != 0){
+                matrix[i,j] *= matrix[i,j];
+            }
+        }
+
+    }
+
+}
+
+int lines = Input();
+int columns = Input();
+int[,] myMatrix = GetRandomMatrix(lines,columns);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+GetChangedMatrix(myMatrix);
+PrintMatrix(myMatrix);
